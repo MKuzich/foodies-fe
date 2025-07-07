e2797b2fd8514b543318c4d3061ca89ad2cc9520
erpokk <maimav56@gmail.com>
2025-07-07 22:27:08 +0200

    Add mock testimonials

--- src/modules/testimonials/components/Testimonials/Testimonials.jsx
+++ src/modules/testimonials/components/Testimonials/Testimonials.jsx
@@ -14,6 +14,24 @@ function Testimonials() {
 
     const value = width <= 768 ? 64 : 80;
 
+    const mockData = [
+        {
+            id: 1,
+            author: "Larry Pageim",
+            comment: "Amazing collection of recipes! The step-by-step instructions make cooking so much easier. I've tried several dishes and they all turned out perfect. Highly recommend!",
+        },
+        {
+            id: 2,
+            author: "Sarah Johnson",
+            comment: "I've been using this app for a few weeks now, and I've already tried several recipes. They all turned out great, and the app's interface is really user-friendly. I'm definitely going to continue using it!",
+        },
+        {
+            id: 3,
+            author: "John Doe",
+            comment: "I've been using this app for a few weeks now, and I've already tried several recipes. They all turned out great, and the app's interface is really user-friendly. I'm definitely going to continue using it!",
+        },
+    ];
+
     return (
         <div className={styles.testimonialsContainer}>
             <p className={styles.testimonialsSubtitle}>What our customer say</p>
@@ -22,8 +40,7 @@ function Testimonials() {
                 <use href="/src/assets/sprite.svg#icon-quote" />
             </svg>
             <h3 className={styles.testimonialsComment}>Thank you for the wonderful recipe for feta pasta with tomatoes and basil. It turned out to be not only tasty, but also incredibly colorful. This has become a favorite family meal!</h3>
-            <p className={styles.testimonialsAuthor}>John Doe</p>
-
+            <p className={styles.testimonialsAuthor}>Larry Pageim</p>
         </div>
 
 
