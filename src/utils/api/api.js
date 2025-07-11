fc363eabd63d4ff02b36c11074a53863e893e680
Anatolii Hrebynakha <hrebynakha@gmail.com>
2025-07-11 23:04:05 +0300

    upd api

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,24 +1,15 @@
 import axios from "axios";
 
-const {
-  VITE_FOODIES_API_ENDPOINT,
-  VITE_FOODIES_API_TOKEN,
-  VITE_FOODIES_AUTH_DEV_MODE,
-} = import.meta.env;
-
-const getToken = () => {
-  const TOKEN = VITE_FOODIES_AUTH_DEV_MODE
-    ? VITE_FOODIES_API_TOKEN
-    : localStorage.getItem("token"); // TODO: rewrite with user auth
-  return TOKEN;
-};
-
 const api = axios.create({
-  baseURL: VITE_FOODIES_API_ENDPOINT,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${getToken()}`,
-  },
+  baseURL: import.meta.env.VITE_FOODIES_API_ENDPOINT,
 });
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
 
 export default api;
