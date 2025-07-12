931117b960342fb60b04a3bce2a043c9a966dbea
Anatolii Hrebynakha <hrebynakha@gmail.com>
2025-07-12 10:10:16 +0300

    fix auth refresh user

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers } from 'redux';
-import { rootReducer } from './root/slice';
-import authReducer from './auth/authSlice';
-import { userReducer } from './users/slice';
-import { categoriesReducer } from './categories/slice';
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { rootReducer } from "./root/slice";
+import authReducer from "./auth/authSlice";
+import { userReducer } from "./users/slice";
+import { categoriesReducer } from "./categories/slice";
 
 const persistConfig = {
-  key: 'auth',
+  key: "auth",
   storage,
-  blacklist: ['authModal'],
+  whitelist: ["userToken"],
 };
 
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
