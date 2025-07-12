9c9b2a6c544ab122e400e996ab8eef6f1394146a
Mykhailo Kuzich <timmkedth@gmail.com>
2025-07-12 09:55:36 +0200

    Added 404 page, extended Container and Button components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,18 +2,33 @@ import React from "react";
 import clsx from "clsx";
 import css from "./Button.module.css";
 
-const Button = ({ children, outlined, inactive, ...props }) => {
+const Button = ({ children, outlined, inactive, isLink, ...props }) => {
   return (
-    <button
-      className={clsx(
-        css.btn,
-        outlined && css.outlined,
-        inactive && css.inactive
+    <>
+      {isLink ? (
+        <a
+          className={clsx(
+            css.btn,
+            outlined && css.outlined,
+            inactive && css.inactive
+          )}
+          {...props}
+        >
+          {children}
+        </a>
+      ) : (
+        <button
+          className={clsx(
+            css.btn,
+            outlined && css.outlined,
+            inactive && css.inactive
+          )}
+          {...props}
+        >
+          {children}
+        </button>
       )}
-      {...props}
-    >
-      {children}
-    </button>
+    </>
   );
 };
 
