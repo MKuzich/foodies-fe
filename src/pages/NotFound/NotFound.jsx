a61ea3e2a6b2b1edfa8b7f3771874dcceb7bbe1b
Mykhailo Kuzich <timmkedth@gmail.com>
2025-07-16 00:15:57 +0200

    Added PathInfo, fixe breadcrumbs

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,25 +1,26 @@
 import React from "react";
-import css from "./NotFound.module.css";
-import Container from "../../components/Container/Container";
+
 import Button from "../../components/Button/Button";
+import Container from "../../components/Container/Container";
 import MainTitle from "../../components/MainTitle/MainTitle";
+import PathInfo from "../../components/PathInfo/PathInfo";
+import css from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <Container className={css.notFoundContainer}>
-      <h1 className={css.title}>404</h1>
-      <MainTitle>Page Not Found</MainTitle>
-      <p className={css.text}>
-        The page you are looking for does not exist or has been moved.
-      </p>
-      <p className={css.suggestion}>
-        Please check the URL or return to the homepage.
-      </p>
+    <>
+      <PathInfo name="Not Found" />
+      <Container className={css.notFoundContainer}>
+        <h1 className={css.title}>404</h1>
+        <MainTitle>Page Not Found</MainTitle>
+        <p className={css.text}>The page you are looking for does not exist or has been moved.</p>
+        <p className={css.suggestion}>Please check the URL or return to the homepage.</p>
 
-      <Button isLink href="/">
-        Go to Homepage
-      </Button>
-    </Container>
+        <Button isLink href="/">
+          Go to Homepage
+        </Button>
+      </Container>
+    </>
   );
 };
 
