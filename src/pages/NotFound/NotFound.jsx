ab6970422cc8d3f8ee1805e0f199785c3ac2e1fc
Mykhailo Kuzich <timmkedth@gmail.com>
2025-07-18 22:13:08 +0200

    Added SEO optimization

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,12 +3,17 @@ import React from "react";
 import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
 import MainTitle from "../../components/MainTitle/MainTitle";
+import Meta from "../../components/Meta/Meta";
 import PathInfo from "../../components/PathInfo/PathInfo";
 import css from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
     <>
+      <Meta
+        title="Page Not Found"
+        description="The page you are looking for does not exist or has been moved."
+      />
       <PathInfo name="Not Found" />
       <Container className={css.notFoundContainer}>
         <h1 className={css.title}>404</h1>
