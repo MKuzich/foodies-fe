62b47e2ff98d1995354cad9228ac145166cffb57
Hrebynakha Anatolii <hrebynakha@gmail.com>
2025-07-19 04:14:30 +0300

    fix wronng spacing

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -68,28 +68,28 @@ const UserInfo = () => {
           </div>
           <div className={css.userName}>{user.name}</div>
           <ul className={css.userInfoList}>
-            <li>
-              <span className={css.userInfoListTitle}>Email: </span>
+            <li className={css.userInfoListItem}>
+              <span className={css.userInfoListTitle}>Email:</span>
               <span className={css.userInfoListValue}>{user.email}</span>
             </li>
-            <li>
-              <span className={css.userInfoListTitle}>Added recipes: </span>
+            <li className={css.userInfoListItem}>
+              <span className={css.userInfoListTitle}>Added recipes:</span>
               <span className={css.userInfoListValue}>{user.createdCount}</span>
             </li>
             {isUserCurrentUser && (
-              <li>
-                <span className={css.userInfoListTitle}>Favorites: </span>
+              <li className={css.userInfoListItem}>
+                <span className={css.userInfoListTitle}>Favorites:</span>
                 <span className={css.userInfoListValue}>{user.favoriteCount}</span>
               </li>
             )}
             {isUserCurrentUser && (
-              <li>
-                <span className={css.userInfoListTitle}>Following: </span>
+              <li className={css.userInfoListItem}>
+                <span className={css.userInfoListTitle}>Following:</span>
                 <span className={css.userInfoListValue}>{user.followingCount}</span>
               </li>
             )}
-            <li>
-              <span className={css.userInfoListTitle}>Followers: </span>
+            <li className={css.userInfoListItem}>
+              <span className={css.userInfoListTitle}>Followers:</span>
               <span className={css.userInfoListValue}>{user.followersCount}</span>
             </li>
           </ul>
